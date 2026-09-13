151d4b45f2f6c36ff013cb1f3a844dc5d185bbbb
Samuel Ramirez <s_ramirezg_0624@outlook.com>
2026-09-13 14:43:54 -0600

    Adición de creación de log con historial de movimientos y obtención del usuario que realizo los movimientos

--- ordenar_archivos_carpetas.py
+++ ordenar_archivos_carpetas.py
@@ -1,14 +1,18 @@
 import os # os nos permite interactuar con el sistema operativo, en este caso usaremos os.path para interactuar con rutas del sistema de archivos
 import shutil # este módulo nos permite realizar operaciones sobre archivos y conjuntos de archivos
+import getpass
 
 from tkinter import Tk, filedialog
+from datetime import datetime
+
+usuario = getpass.getuser() # Se captura al usuario que se logueo a la máquina y que está usando este programa
 
 ventana = Tk() # creamos una ventana
 ventana.withdraw() # nos ayuda a quitar la ventana de cmd que aparece con la ventana de selección del archivo
 
 rutaTrabajo = filedialog.askdirectory(title="Seleccionar carpeta") # obtenemos el directorio y le damos un nombre a la ventana
 
-extensiones = { # Creamos el diccionario que contiene la extensiones
+extensiones = { # Creamos el diccionario que contiene la extsensiones
     ".jpg" : "Imagenes",
     ".png" : "Imagenes",
     ".pdf" : "PDFs",
@@ -32,10 +36,14 @@ for archivo in os.listdir(rutaTrabajo):
         nombre, ext = os.path.splitext(archivo) # Separa el nombre del archivo de su extensión
         ext = ext.lower() #deja en minúsculas todas las extensiones en caso de que haya algunas con mayúsculas
 
-        if ext in extensiones: #si la extensión obtenida está en el diccionario de extensiones
-            destino = os.path.join(rutaTrabajo, extensiones[ext], archivo) # Se une la ruta con la carpeta y luego con el nombre del archivo
+        if ext in extensiones: # si la extensión obtenida está en el diccionario de extensiones
+            destino = os.path.join(rutaTrabajo, extensiones[ext], archivo) # Se une la ruta con el nombre de carpeta proveniente de la colección extensiones y luego esto se une con el nombre del archivo
             shutil.move(rutaArchivo, destino)
 
+            with open(os.path.join(rutaTrabajo, "log_movimientos.txt"), "a", encoding="utf-8") as log: # Creamos un bloque with y con el manejamos un objeto llamdo "log"
+                log.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Usuario: {usuario}  - Archivo: {archivo} movido a  {destino}\n") # Escribimos la fecha y hora en que se movió así como el destino al que se movió
+
+                
 '''
 Fuentes de información
 https://docs.python.org/3/library/os.html
